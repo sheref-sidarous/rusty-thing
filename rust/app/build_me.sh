83afc892b8550df71951b8459edc740275222844
Sheref Sidarous <sherefyounan@gmail.com>
2020-11-12 06:22:20 +1100

    scripted the workaround and gave rust app a better name

--- rust/app/build_me.sh
+++ rust/app/build_me.sh
@@ -1,1 +1,6 @@
+#! /bin/sh
+
 cargo build --target=thumbv7m-none-eabi
+
+# a workaround becuase rust static libs include compiler builtins by default, chec docs/notes.md
+ar vd target/thumbv7m-none-eabi/debug/librust_app.a compiler_builtins-938fa3b5396dcc28.compiler_builtins.xikbgi4a-cgu.150.rcgu.o
