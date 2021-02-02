08644e5823d98dd7c9a4e33d4d0a30b2bc9026ae
Sheref Sidarous <sherefyounan@gmail.com>
2021-02-02 19:43:22 +1100

    adding more stuff into rust app

--- rust/app/src/lib.rs
+++ rust/app/src/lib.rs
@@ -23,15 +23,31 @@ extern {
     fn InitTerm() -> u32;
     fn UART_control(handle : u32, config : u16 , arg3 : u32);
     fn UART_writePolling(handle : u32, ptr_to_str : u32, len : u32);
-}
 
-fn print(message : &str) {
-    unsafe {
-        raw_print(message);
-    }
+    fn GPIO_init();
+    fn SPI_init();
+    fn Timer_init();
+    fn ti_net_SlNet_initConfig() -> i32;
+    fn WifiInit() -> i32;
+
+    fn GPIO_write(arg1 : u32, arg2 : u32);
+
 }
 
 static mut uartHandle : u32 = 0;
+
+fn print(message : &str)
+{
+    unsafe {
+        UART_writePolling(uartHandle, message.as_ptr() as u32, message.len() as u32);
+    }
+}
+
+static LOG_ERROR : fn(&str)->() = print;
+static CONFIG_GPIO_LED_0 : u32 = 2;
+static CONFIG_GPIO_LED_1 : u32 = 3;
+static CONFIG_GPIO_LED_2 : u32 = 4;
+static CONFIG_GPIO_LED_OFF : u32 = 0;
 
 #[no_mangle]
 pub extern "C" fn rusty_entry() {
@@ -43,9 +59,26 @@ pub extern "C" fn rusty_entry() {
 fn main_thread () {
 
     unsafe {
-
         uartHandle = InitTerm();
         UART_control(uartHandle, 4, 0);
+
+        GPIO_init();
+        SPI_init();
+        Timer_init();
+    
+        if ti_net_SlNet_initConfig() != 0 {
+            LOG_ERROR("Failed to initialize SlNetSock\n\r");
+            loop {};
+        }
+    
+        if WifiInit() < 0 {
+            LOG_ERROR("Failed to initialize WiFi\n\r");
+            loop {};
+        }
+    
+        GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_OFF);
+        GPIO_write(CONFIG_GPIO_LED_1, CONFIG_GPIO_LED_OFF);
+        GPIO_write(CONFIG_GPIO_LED_2, CONFIG_GPIO_LED_OFF);
 
     }
 
@@ -56,9 +89,5 @@ fn main_thread () {
 
 }
 
-unsafe fn raw_print(message : &str)
-{
-    UART_writePolling(uartHandle, message.as_ptr() as u32, message.len() as u32);
-}
 
 
